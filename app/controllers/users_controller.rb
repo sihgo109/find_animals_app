e10857ff2591ada9be0f6a37b57ac16ee4df210e
sihgo109 <89370531+sihgo109@users.noreply.github.com>
2021-10-10 19:25:31 +0900

    ユーザ編集機能実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,4 +6,22 @@ class UsersController < ApplicationController
     # @nickname = user.nickname
     # @animals = user.animals
   end
+
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:nickname, :email, :phone_number, :last_name, :first_name, :age, :job, :prefecturre, :profile)
+  end
+
 end

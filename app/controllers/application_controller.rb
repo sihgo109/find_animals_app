25dd1ca430205c9da6352f9d38d62f438645c2f0
sihgo109 <89370531+sihgo109@users.noreply.github.com>
2021-10-10 14:10:02 +0900

    ユーザ管理機能実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname,:profile,:prefecture,:phone_number, :last_name, :first_name, :age, :job, :profile])
+  end
 end
---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :phone_number, presence: true
+  validates :nickname, presence: true
+  validates :last_name, presence: true
+  validates :first_name, presence: true
+  validates :age, presence: true
+  validates :job, presence: true
+  validates :profile, presence: true
+  validates :prefecture, presence: true
+
+end

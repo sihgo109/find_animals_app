6b54dcd696da1f1e9f2b1ae7fb6329e1fcd15560
sihgo109 <89370531+sihgo109@users.noreply.github.com>
2021-10-10 15:39:56 +0900

    削除機能実装

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,5 +1,6 @@
 class AnimalsController < ApplicationController
   def index
+    @animals = Animal.all
   end
 
   def new
@@ -14,11 +15,37 @@ class AnimalsController < ApplicationController
       render :new
     end
   end
-end
+
+  def show
+    @animal = Animal.find(params[:id])
+  end
+
+  def edit
+    @animal = Animal.find(params[:id])
+  end
+
+  def update
+    @animal = Animal.find(params[:id])
+    if @animal.update(animal_params)
+      redirect_to animal_path  
+   else
+     render :edit
+   end
+  end
+
+  def destroy
+    @animal = Animal.find(params[:id])
+    @animal.destroy
+      redirect_to root_path
+  end
 
 
-private
+
+
+  private
 
   def animal_params
     params.require(:animal).permit(:name, :charm_point, :genre, :gender, :birthday, :age, :prefecture, :image).merge(user_id: current_user.id)
   end
+
+end

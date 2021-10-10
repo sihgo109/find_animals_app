601acc88ea21321af8206745cb2327f9c4e48af2
sihgo109 <89370531+sihgo109@users.noreply.github.com>
2021-10-11 01:11:52 +0900

    DM機能の実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,33 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @users = User.all
+  end
+
   def show
     @user = User.find(params[:id])
-    # @nickname = user.nickname
-    # @animals = user.animals
+    @currentUserEntry=Entry.where(user_id: current_user.id)
+    @userEntry=Entry.where(user_id: @user.id)
+    if @user.id == current_user.id
+    else
+      @currentUserEntry.each do |cu|
+        @userEntry.each do |u|
+          if cu.room_id == u.room_id then
+            @isRoom = true
+            @roomId = cu.room_id
+          end
+        end
+      end
+      if @isRoom
+      else
+        @room = Room.new
+        @entry = Entry.new
+      end
+    end
   end
+    
+  
 
   def new
     @user = User.new

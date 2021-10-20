8fca0731b79d7f8aae4d11a8f36b8b116015ec9e
sihgo109 <89370531+sihgo109@users.noreply.github.com>
2021-10-20 09:29:40 +0900

    Revert "Merge pull request #10 from sihgo109/aboutページ作成"
    This reverts commit eb9594e29e3730741f28dbf344e1e9aedead9b56, reversing
    changes made to be925c017931757fc4ffbad470481e228be66645.

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,6 +1,6 @@
 class AnimalsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show, :about]
-  before_action :move_to_index, except: [:index, :show, :about]
+  before_action :authenticate_user!, except: [:index, :show]
+  before_action :move_to_index, except: [:index, :show]
 
   def index
     @animals = Animal.includes(:user).order("created_at DESC")
@@ -8,9 +8,6 @@ class AnimalsController < ApplicationController
 
   def new
     @animal = Animal.new
-  end
-
-  def about
   end
 
   def create
